c4944d0b694acc037490d5520a53d9c5d213ac3a
Rowdy <mroddy5280@gmail.com>
2023-09-09 19:26:50 -0500

    Added UI lib

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Repo Rewards</h1>
+          <p className="py-6">
+            Fund contributors and maintainers of open source projects you use
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
